456ec1a2218b0872dfa15fb0820d43062aca8f0e
Eugene <131405736+EvgenBilous@users.noreply.github.com>
2023-09-24 22:39:59 -0500

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,12 @@
+// const delayEl = document.querySelector('[name="delay"]');
+
+const refs = {
+  delayEl: document.querySelector('[name="delay"]'),
+  stepEl: document.querySelector('[name="step"]'),
+  amountEl: document.querySelector('[name="amount"]'),
+};
+//console.log(refs.delayEl, refs.amountEl, refs.stepEl);
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
@@ -6,3 +15,4 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+createPromise(1000, refs.delayEl.value);
