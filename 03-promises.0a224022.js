526726b4221aaa6c74548c6a5b75742438bb60b0
EvgenBilous <131405736+EvgenBilous@users.noreply.github.com>
2023-09-26 16:59:12 +0000

    Deploying to gh-pages from @ EvgenBilous/goit-js-hw-09@03ebb8606278eb47e51ca4fd094b8f31caa6e988 🚀

--- 03-promises.0a224022.js
+++
@@ -1,2 +0,0 @@
-[document.querySelector('[name="delay"]'),document.querySelector('[name="step"]'),document.querySelector('[name="amount"]')][0].value,Math.random();
-//# sourceMappingURL=03-promises.0a224022.js.map
